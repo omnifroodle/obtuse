5148f4346edc292a836be22425a5a495afdb377d
Paul Venuto <pavenuto@gmail.com>
2013-06-13 19:59:25 -0400

    wrapping quote in h1 tag

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Obtuse::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'witticisms#random'
+  # root 'witticisms#random'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
