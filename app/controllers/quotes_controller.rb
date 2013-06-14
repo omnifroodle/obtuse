b6bcdc0584578203a95b798ce346f7b0e632214a
Bob Larrick <larrick@gmail.com>
2013-06-13 20:32:03 -0400

    Put related words together

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,6 +2,9 @@ class QuotesController < ApplicationController
 
   def index
     @quote = Markov.new.gimme(6)
+
+    @previous_source = @quote.first.source
+    @previous_index = @quote.first.index
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+  def do_break(o)
+    !same_source(o) || !successive_index(o)
+  end
+
+  def should_close_span(o)
+    foo = !same_source(o)
+    bar = !successive_index(o)
+    tom = foo || bar
+    tom
+  end
+
+  def in_a_row(o)
+    same_source(o) && successive_index(o)
+  end
+
+  def successive_index(o)
+#    binding.pry
+    (@previous_index.succ == o.index)
+  end
+
+  def same_source(o)
+    (o.source == @previous_source)
+  end
 end
