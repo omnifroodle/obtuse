1b3cf53e89683aceb65478b4c73f1e0b8c42665a
Bob Larrick <larrick@gmail.com>
2013-06-13 21:19:31 -0400

    asdf

--- app/assets/javascripts/witticisms.js
+++ app/assets/javascripts/witticisms.js
@@ -16,3 +16,18 @@ function createWitticism(){
   $.post("/witticisms", {"witticism": {"quote": foo}});
   return false;
 }
+
+$(document).keydown(function(e){
+    console.log(e.keyCode);
+   if (e.keyCode == 83) {
+     createWitticism();
+    }else if(e.keyCode == 13){
+     createWitticism();
+    }else if(e.keyCode == 39){
+      location.reload(true);
+    }else if(e.keyCode == 40){
+      createWitticism();
+    }
+
+
+});
